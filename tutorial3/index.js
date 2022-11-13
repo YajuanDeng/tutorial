89ce130f4f716c45399a0cdbf3c7de2fd694c7cd
YajuanDeng <clairedeng713@gmail.com>
2022-11-13 19:18:59 +1000

    practice

--- tutorial3/index.js
+++ tutorial3/index.js
@@ -4,24 +4,43 @@
     const heightInput = document.querySelector('#height-input').value
     const weightInput = document.querySelector('#weight-input').value
     //如果输入的是空值则需要弹窗提醒
-    if (height === '' || weightInput === '') {
+    if (heightInput === '' || weightInput === '') {
         alert('Please put your value in');
         return;
     }
-    // console.log('height', heightInput);
-    // console.log('weight', weightInput);
+    console.log('height', heightInput);
+    console.log('weight', weightInput);
 
-    const heightValue = Number.parseInt(heightInput)
-    const weightValue = Number.parseInt(weightInput)
+    const heightValue = +heightInput
+    const weightValue = +weightInput
 
-    const BMI = weightValue / (heightValue * heightValue)
+    // const BMI = weightValue / (heightValue * heightValue)
+    const BMI = weightValue/heightValue/heightValue
     const BMIString = BMI.toFixed(2)
-
+    
+    const BMIString1 = +BMIString
+    console.log('BMIString1',typeof(BMIString1));
     console.log(`Your BMI is ${BMIString}`);
 
     //可以对结果进行判断，太轻太重或者刚好？
 
-    const resultNode = document.querySelector('#result');
+    
+    if (BMIString1 < 18){
+        print = 'Underweight: BMI is below the 5th percentile age, gender, and height.'
+    } else if (BMIString1 < 24) {
+        print = 'Healthy weight: BMI is equal to or greater than the 5th percentile and less than the 85th percentile for age, gender, and height.'
+    } else if (BMIString1 < 34) {
+        print = 'Overweight: BMI is at or above the 85th percentile but less than the 95th percentile for age, gender, and height.'
+    } else {
+        print = 'Obese: BMI is at or above the 95th percentile for age, gender, and height.'
+    }  
+    
+    console.log(`${print}`);
+    const caution = document.querySelector('#caution');
+    const resultNode = document.querySelector('#yourResult');
+
     resultNode.innerHTML = `Your BMI is ${BMIString}`;
+    caution.innerHTML = `${print}`;
  })
 
+ 
